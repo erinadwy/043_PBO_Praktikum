d9b6f408532db6a96ff7e7fe50189e48b856488d
erinadwy <158986567+erinadwy@users.noreply.github.com>
2024-08-22 17:34:23 +0700

    Initial commit

---
+++ src/Soal_2.java
@@ -0,0 +1,7 @@
+public class Soal_2 {
+    public static void main(String[] args){
+        int i = 50;
+        String s = (i<40)?"life":(i>50)?"universe":"everything";
+        System.out.println(s);
+    }
+}
